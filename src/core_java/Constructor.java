7db7fe681d137025d495d7719620c897b91d0be6
thakurboy0555 <as5162825@gmail.com>
2025-03-17 16:46:16 +0530

    Constructor in java

---
+++ src/core_java/Constructor.java
@@ -0,0 +1,13 @@
+package core_java;
+
+public class Constructor {
+
+	Constructor(){
+		System.out.println("hello form constructor");
+	}
+	public static void main(String[] args) {
+		new Constructor();
+
+	}
+
+}
